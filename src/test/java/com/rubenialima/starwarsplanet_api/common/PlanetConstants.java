d7b4335537f4539ba1c29ff37bdb112728d240cd
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-08-09 21:21:44 -0300

    Criação das classes PlanetServiceTest e  PlanetContants

---
+++ src/test/java/com/rubenialima/starwarsplanet_api/common/PlanetConstants.java
@@ -0,0 +1,7 @@
+package com.rubenialima.starwarsplanet_api.common;
+
+import com.rubenialima.starwarsplanet_api.domain.Planet;
+
+public class PlanetConstants {
+    public static final Planet PLANET = new Planet("name","climate", "terrain");
+}
--- src/test/java/com/rubenialima/starwarsplanet_api/domain/PlanetServiceTest.java
+++ src/test/java/com/rubenialima/starwarsplanet_api/domain/PlanetServiceTest.java
@@ -1,7 +1,16 @@
 package com.rubenialima.starwarsplanet_api.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
-    public void test(){
-        
+    @Autowired
+    PlanetService planetService;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet(){
+
     }
 }
