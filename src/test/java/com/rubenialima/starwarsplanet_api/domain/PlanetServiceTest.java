889d533989fc9de9d0ed10beab6a81e983116afd
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-08-13 23:39:49 -0300

    implementando o  assertThat() no metodo createPlanet_WithValidData_ReturnsPlanet()

--- src/test/java/com/rubenialima/starwarsplanet_api/domain/PlanetServiceTest.java
+++ src/test/java/com/rubenialima/starwarsplanet_api/domain/PlanetServiceTest.java
@@ -3,6 +3,7 @@ package com.rubenialima.starwarsplanet_api.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import static com.rubenialima.starwarsplanet_api.common.PlanetConstants.PLANET;
 
@@ -13,6 +14,7 @@ public class PlanetServiceTest {
 
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet(){
-        planetService.create(PLANET);
+        Planet sut = planetService.create(PLANET);
+        assertThat(sut).isEqualTo(PLANET);
     }
 }
