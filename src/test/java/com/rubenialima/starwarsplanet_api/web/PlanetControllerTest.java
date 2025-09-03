9fec759e28a86499f6a1b3a34ad988f191aaa716
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-09-02 22:56:42 -0300

    Criação da classe PlanetControllerTest

---
+++ src/test/java/com/rubenialima/starwarsplanet_api/web/PlanetControllerTest.java
@@ -0,0 +1,12 @@
+package com.rubenialima.starwarsplanet_api.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+    @Test
+    public void createdPlanet_WithValidData_ReturnsCreated(){
+
+    }
+}
