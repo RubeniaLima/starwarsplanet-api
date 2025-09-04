ed51ddd2b09e9724ea82b3bda9a49bf51877dcba
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-09-03 23:46:53 -0300

    implementando o metodo  createdPlanet_WithValidData_ReturnsCreated

--- src/test/java/com/rubenialima/starwarsplanet_api/web/PlanetControllerTest.java
+++ src/test/java/com/rubenialima/starwarsplanet_api/web/PlanetControllerTest.java
@@ -1,12 +1,30 @@
 package com.rubenialima.starwarsplanet_api.web;
 
+import static com.rubenialima.starwarsplanet_api.common.PlanetConstants.PLANET;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.URI;
 
 @WebMvcTest(PlanetController.class)
 public class PlanetControllerTest {
-    @Test
-    public void createdPlanet_WithValidData_ReturnsCreated(){
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void createdPlanet_WithValidData_ReturnsCreated() throws Exception {
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET)))
+                .andExpect(status().isCreated());
     }
 }
