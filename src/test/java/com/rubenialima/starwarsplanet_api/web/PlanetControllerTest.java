f08f33b8517ccb072324c1fe34baf6ef34c78d98
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-09-04 20:28:02 -0300

    implementando o metodo createPlanet_WithValidData_ReturnsCreated com o when..thenReturn

--- src/test/java/com/rubenialima/starwarsplanet_api/web/PlanetControllerTest.java
+++ src/test/java/com/rubenialima/starwarsplanet_api/web/PlanetControllerTest.java
@@ -1,15 +1,18 @@
 package com.rubenialima.starwarsplanet_api.web;
 
 import static com.rubenialima.starwarsplanet_api.common.PlanetConstants.PLANET;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rubenialima.starwarsplanet_api.domain.PlanetService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
@@ -21,11 +24,15 @@ public class PlanetControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private PlanetService planetService;
+
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
     public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
+        when(planetService.create(PLANET)).thenReturn(PLANET);
         mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET)))
                 .andExpect(status().isCreated())
                 .andExpect((ResultMatcher) jsonPath("$").value(PLANET));
