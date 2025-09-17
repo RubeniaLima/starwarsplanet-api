5fb084e8fa65bda3a81babb5479d3142b16c5b33
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-09-16 21:41:26 -0300

    Criação do metodo findByName na classe PlanetRepository

--- src/main/java/com/rubenialima/starwarsplanet_api/domain/PlanetRepository.java
+++ src/main/java/com/rubenialima/starwarsplanet_api/domain/PlanetRepository.java
@@ -2,5 +2,8 @@ package com.rubenialima.starwarsplanet_api.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlanetRepository extends JpaRepository<Planet, Long> {
+    Optional<Planet> findByName(String name);
 }
