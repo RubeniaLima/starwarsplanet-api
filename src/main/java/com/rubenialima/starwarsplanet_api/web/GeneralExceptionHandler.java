7c41dc1ef303b89704322426681475d5aec7e577
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-10-17 19:24:24 -0300

    Correção de Bug no metodo createPlanet_WithInvalidData_ReturnsBadRequest

--- src/main/java/com/rubenialima/starwarsplanet_api/web/GeneralExceptionHandler.java
+++ src/main/java/com/rubenialima/starwarsplanet_api/web/GeneralExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.rubenialima.starwarsplanet_api.web;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,7 +12,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    protected ResponseEntity<Object> handleMethodArgumentNotValid
+            (MethodArgumentNotValidException ex,
+             HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
     }
 }
