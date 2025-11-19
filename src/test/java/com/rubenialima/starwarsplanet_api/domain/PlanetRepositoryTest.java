c11adf568e9bc3992930dcc3835c074b53059625
rubenia.lima2608@gmail.com <rubenia.lima2608@gmail.com>
2025-11-18 22:37:51 -0300

    Criacao do metodo de teste removePlanet_WithUnexistingId_ThrowsException na classe PlanetRepositoryTest

--- src/test/java/com/rubenialima/starwarsplanet_api/domain/PlanetRepositoryTest.java
+++ src/test/java/com/rubenialima/starwarsplanet_api/domain/PlanetRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Example;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -122,5 +123,9 @@ public class PlanetRepositoryTest {
         Planet removedPlanet = testEntityManager.find(Planet.class, planet.getId());
 
         assertThat(removedPlanet).isNull();
+    }
+    @Test
+    public void removePlanet_WithUnexistingId_ThrowsException(){
+        assertThatThrownBy(()->planetRepository.deleteById(1L)).isInstanceOf(EmptyResultDataAccessException.class);
     }
 }
